c215871cc71ecc6037bd73df4712d43e1a46e29d
Mura910 <murasaki1934@gmail.com>
2022-12-08 23:45:38 +0900

    実装途中

--- menu-app/src/app/menu/infrastructure/menu-data-loader.serviceimpl.ts
+++ menu-app/src/app/menu/infrastructure/menu-data-loader.serviceimpl.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { MenuData } from 'src/app/shared/domain/menu-data';
+import { Category, MenuData } from 'src/app/shared/domain/menu-data';
 import { MenuDataLoader } from 'src/app/shared/domain/menu-data-loader';
 import { MenuDataLoaderService } from '../domain/menu-data-loader.service';
 
@@ -11,5 +11,19 @@ export class MenuDataLoaderServiceImpl implements MenuDataLoaderService {
   constructor(private menuDataLoader: MenuDataLoader) { }
   loadMenu(): MenuData[] {
     return this.menuDataLoader.loadMenu();
+  }
+  //TODO動いていない。要修正
+  getFoods(menuList: MenuData[]): MenuData[] {
+    let foodList = menuList.filter(data =>  data.category === Category.food);
+    console.log(foodList.length)
+    return foodList;
+  }
+  getSweets(menuList: MenuData[]): MenuData[] {
+    let sweetList = menuList.filter(data => data.category == Category.sweet);
+    return sweetList;
+  }
+  getDrinks(menuList: MenuData[]): MenuData[] {
+    let drinkList = menuList.filter(data => data.category == Category.drink);
+    return drinkList;
   }
 }
